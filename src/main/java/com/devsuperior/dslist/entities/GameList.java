b37c8f434556b905b6e98fe697af79a599423bfc
emersonpessoa <eps011108@gmail.com>
2025-08-31 21:14:16 -0300

    feat: Adicionado GameList :rocket:

--- src/main/java/com/devsuperior/dslist/entities/GameList.java
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -2,7 +2,17 @@ package com.devsuperior.dslist.entities;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_game_list")
 public class GameList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
@@ -30,15 +40,20 @@ public class GameList {
         this.name = name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        GameList gameList = (GameList) o;
-        return Objects.equals(id, gameList.id) && Objects.equals(name, gameList.name);
-    }
+   @Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GameList other = (GameList) obj;
+		return Objects.equals(id, other.id);
+	}
 }
