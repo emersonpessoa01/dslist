f8c0e0c58140d08ca30de3cf3395ff5255f3467e
emersonpessoa <eps011108@gmail.com>
2025-10-04 17:03:07 -0300

    feat: Adicionado método findById no GameService :sparkler:

--- src/main/java/com/devsuperior/dslist/Services/GameService.java
+++ src/main/java/com/devsuperior/dslist/Services/GameService.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dslist.Services;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,10 +14,24 @@ import java.util.List;
 public class GameService {
     @Autowired
     private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
 //        List<GameMinDTO> dto = result.stream().map(x ->new GameMinDTO(x)).toList();
 //          return dto;
         return result.stream().map(x ->new GameMinDTO(x)).toList();
+    }
+
+   @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();//Para pegarGame dento do Optional
+        // Converter o Objto Game para GameDTO
+        //GameDTO dto = new GameDTO(result);
+        return new GameDTO(result);
+        //Garantindo os principios das transaçõs:acid
+        //Atomica, consitente,isolada e  duravel
+
+
     }
 }
