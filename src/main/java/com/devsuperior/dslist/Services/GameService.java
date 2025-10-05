c4d8804fe8a0aab928ce2c7aefea9dea3df20f3b
emersonpessoa <eps011108@gmail.com>
2025-10-05 15:25:13 -0300

    feat:Criado método findByList no GameService :rocket:

--- src/main/java/com/devsuperior/dslist/Services/GameService.java
+++ src/main/java/com/devsuperior/dslist/Services/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.Services;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -33,5 +34,11 @@ public class GameService {
         //Atomica, consitente,isolada e  duravel
 
 
+    }
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        //Converter projection em dto
+        return result.stream().map(x ->new GameMinDTO(x)).toList();
     }
 }
