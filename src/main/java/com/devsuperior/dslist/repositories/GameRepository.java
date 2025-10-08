869e35295c491f1365a282841df2a9b3dbd45023
emersonpessoa <eps011108@gmail.com>
2025-10-08 20:43:39 -0300

    fix: Corrigido o campo game_year do GameRepository :rocket:

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -8,13 +8,18 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-    @Query(nativeQuery = true, value = """
-		SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
-		tb_game.short_description AS shortDescription, tb_belonging.position
-		FROM tb_game
-		INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
-		WHERE tb_belonging.list_id = :listId
-		ORDER BY tb_belonging.position
-			""")
+
+	@Query(nativeQuery = true, value = """
+        SELECT g.id,
+               g.title,
+               g.game_year AS "year",
+               g.img_url AS imgUrl,
+               g.short_description AS shortDescription,
+               b.position
+        FROM tb_game g
+        INNER JOIN tb_belonging b ON g.id = b.game_id
+        WHERE b.list_id = :listId
+        ORDER BY b.position
+    """)
 	List<GameMinProjection> searchByList(Long listId);
 }
