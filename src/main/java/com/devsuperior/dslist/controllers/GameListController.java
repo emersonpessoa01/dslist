9b3f3127b98cd84d83f4178d84a91e61ac465c68
emersonpessoa <eps011108@gmail.com>
2025-11-02 12:50:44 -0300

    feat: Atualizado endpoint do métodomove no GameListController :rocket:

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -29,7 +29,7 @@ public class GameListController {
         List<GameMinDTO> result = gameService.findByList(listId);
         return result;
     }
-    @PostMapping(value = "/{listId}/games")
+    @PostMapping(value = "/{listId}/replacement")
     public void move(@PathVariable Long listId,@RequestBody ReplacementDTO body ) {
         gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
